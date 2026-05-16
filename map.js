09184e80b0a5e81248a8093ede42cfcfd8eb8478
andrewyounucsd <asyoun@ucsd.edu>
2026-05-15 19:21:39 -0700

    added map.css and map to map.js

--- map.js
+++ map.js
@@ -2,3 +2,14 @@ import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 console.log('Mapbox GL JS Loaded:', mapboxgl);
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYXN5b3VuIiwiYSI6ImNtcDdwdjZjMDA0ZzEydXBvenk3MTRjejYifQ.h_aI0GxCLMjcp7joEHDZ_Q';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v12',
+  center: [-71.09415, 42.36027],
+  zoom: 12,
+  minZoom: 5,
+  maxZoom: 18,
+});
