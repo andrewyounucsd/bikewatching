ad0861bf2b60bb39a5c514c6f3d733973976ea9c
andrewyounucsd <asyoun@ucsd.edu>
2026-05-15 19:19:49 -0700

    added map.js

---
+++ map.js
@@ -0,0 +1,4 @@
+import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+
+console.log('Mapbox GL JS Loaded:', mapboxgl);
